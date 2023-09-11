03dda7bbd86ffe693603392930d72f8be8fca786
Aleksid <aleksidys@gmail.com>
2023-09-11 08:07:06 +0300

    11.09.2023

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,27 +1,27 @@
-document.querySelector("form").addEventListener("submit", function (event) {
-    event.preventDefault();
-  });
-  
-  const taskInput = document.getElementById("task__input");
-  const taskList = document.getElementById("tasks__list");
-  
-  document.addEventListener("keydown", function (event) {
-    if (event.which === 13) {
-      const task = document.createElement("div");
-      task.classList.add("task");
-      const title = document.createElement("div");
-      title.classList.add("task__title");
-      title.textContent = taskInput.value;
-      const removeBtn = document.createElement("a");
-      removeBtn.classList.add("task__remove");
-      removeBtn.textContent = "×";
-      removeBtn.href = "#";
-      removeBtn.addEventListener("click", function () { 
-        task.remove(); 
-      });
-      task.appendChild(title);
-      task.appendChild(removeBtn);
-      taskList.appendChild(task);
-      taskInput.value = "";
-    }
-  });
+
+const taskInput = document.getElementById("task__input");
+const taskList = document.getElementById("tasks__list");
+const addBtn = document.getElementById("tasks__add");
+
+addBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  const taskText = taskInput.value.trim();
+  if (taskText !== "") {
+    const task = document.createElement("div");
+    task.classList.add("task");
+    const title = document.createElement("div");
+    title.classList.add("task__title");
+    title.textContent = taskText;
+    const removeBtn = document.createElement("a");
+    removeBtn.classList.add("task__remove");
+    removeBtn.textContent = "×";
+    removeBtn.href = "#";
+    removeBtn.addEventListener("click", function () { 
+      task.remove(); 
+    });
+    task.appendChild(title);
+    task.appendChild(removeBtn);
+    taskList.appendChild(task);
+    taskInput.value = "";
+  }
+});
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,19 +1,24 @@
-
 const elements = document.getElementsByClassName("has-tooltip");
 Array.from(elements).forEach((element) => {
-   element.addEventListener("click", function(event) {
-    event.preventDefault(); 
-    const tooltip = document.createElement("div");
-    tooltip.classList.add("tooltip")
-    tooltip.classList.toggle("tooltip_active");
-    tooltip.innerHTML = element.getAttribute("title")
-    element.insertAdjacentElement("afterend", tooltip);
-    const elementCoords = element.getBoundingClientRect();
-    const tooltipCoords = tooltip.getBoundingClientRect();
-    const left = elementCoords.left + (element.offsetWidth - tooltip.offsetWidth) / 2;
-    const top = elementCoords.top + element.offsetHeight;
-    tooltip.style.left = left + 'px';
-    tooltip.style.top = top + 'px';
-  });
+  element.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    let nextElement = event.target.nextElementSibling;
+    if (nextElement && nextElement.className === 'tooltip tooltip_active') {
+      nextElement.remove();
+    } else {
+      const tooltip = document.createElement("div");
+      tooltip.classList.toggle("tooltip");
+      tooltip.classList.toggle("tooltip_active");
+      tooltip.innerHTML = element.getAttribute("title");
+      element.insertAdjacentElement("afterend", tooltip);
+      const elementCoords = element.getBoundingClientRect();
+      const tooltipCoords = tooltip.getBoundingClientRect();
+      const left = elementCoords.left + (element.offsetWidth - tooltip.offsetWidth) / 2;
+      const top = elementCoords.top + element.offsetHeight;
+      tooltip.style.left = left + 'px';
+      tooltip.style.top = top + 'px';
+    }
+  });
 });
+
 
